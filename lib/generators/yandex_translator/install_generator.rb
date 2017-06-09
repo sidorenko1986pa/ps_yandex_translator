ab06f0bdc3bf07a76406bff65a17fbb1e4c52bd2
FoX <p.sidorenko@leko.in>
2017-06-09 23:03:47 +0300

    generator 3

--- lib/generators/yandex_translator/install_generator.rb
+++ lib/generators/yandex_translator/install_generator.rb
@@ -1,7 +1,7 @@
 module YandexTranslator
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root(File.expand_path(File.dirname(__FILE__))
+      source_root(File.expand_path(File.dirname(__FILE__)))
       def copy_initializer
         copy_file 'conf.rb', 'config/initializers/yandex_translator.rb'
       end
